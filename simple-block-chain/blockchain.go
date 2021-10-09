648787b8cbe43889713655cd8061aef70d846eba
nikasakana <nikasakana@gmail.com>
2021-10-09 13:00:27 +0400

    added blockchain validation

--- simple-block-chain/blockchain.go
+++ simple-block-chain/blockchain.go
@@ -9,14 +9,21 @@ import (
 )
 
 const (
-	START   = "START"
-	NO_HASH = "NO_HASH"
+	START            = "START"
+	NO_HASH          = "NO_HASH"
+	VALIDATION_OK    = 1
+	VALIDATION_ERROR = 2
 )
 
 type Node struct {
 	Next     *Node
 	Value    string
 	NextHash string
+}
+
+type ValidationResult struct {
+	Status  int
+	Message string
 }
 
 func isStringValid(str string) bool {
@@ -30,30 +37,53 @@ func getHashOfValue(value string) string {
 	return sha
 }
 
-func getStdIn() {
+func initBlockChain(head *Node) {
 	scanner := bufio.NewScanner(os.Stdin)
-	head := &Node{Next: nil, Value: START, NextHash: NO_HASH}
 	current := head
 	for scanner.Scan() {
 		fmt.Print("Please enter value for next block in blockchain: ")
 		inputValue := scanner.Text()
 		if isStringValid(inputValue) {
 			next := &Node{Next: nil, Value: inputValue, NextHash: NO_HASH}
-			current.NextHash = getHashOfValue(current.Value)
+			current.NextHash = getHashOfValue(inputValue)
 			current.Next = next
 			current = next
 		} else {
 			break
 		}
 	}
+
+}
+
+func validateBlockChain(head *Node) ValidationResult {
 	temp := head
-	for temp != nil {
-		fmt.Println(temp.Value)
-		fmt.Println(temp.NextHash + " \n")
+	result := ValidationResult{Status: VALIDATION_OK, Message: "blockchain is valid"}
+	hash := head.NextHash
+	if temp.Next != nil {
 		temp = temp.Next
 	}
+	for temp != nil {
+		currentNodeHash := getHashOfValue(temp.Value)
+		if currentNodeHash != hash {
+			fmt.Println(currentNodeHash + "  " + hash)
+			result.Status = VALIDATION_ERROR
+			result.Message = "Validation error blockchain was tempered"
+			return result
+		}
+		hash = temp.NextHash
+		temp = temp.Next
+	}
+	return result
+}
+
+func printResults(validationResult ValidationResult) {
+	fmt.Println("Validation result: ", validationResult.Status)
+	fmt.Println("Validatoin message: " + validationResult.Message)
 }
 
 func main() {
-	getStdIn()
+	head := &Node{Next: nil, Value: START, NextHash: NO_HASH}
+	initBlockChain(head)
+	blockChainValidationResult := validateBlockChain(head)
+	printResults(blockChainValidationResult)
 }
